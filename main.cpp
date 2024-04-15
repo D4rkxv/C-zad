154d767eb5268dc62564e815207f39c3b888fa44
D4rkxv <patrykmalczyk11@gmail.com>
2024-04-15 21:26:46 +0200

    1-10

--- main.cpp
+++ main.cpp
@@ -1,140 +1,137 @@
 #include <iostream>
-#include <valarray>
+#include <cmath>
 
 using namespace std;
-class pprospopadloscian{
+//Zad.1,2
+class Prostopadloscian {
 public:
-    int a,b,c;
-
-    pprospopadloscian(int bok1,int bok2, int bok3) {
+    int a, b, c;
+    Prostopadloscian(int bok1, int bok2, int bok3) {
         a = bok1;
         b = bok2;
         c = bok3;
+    }
 
+    int liczPole() {
+        return (2 * a * b) + (2 * b * c) + (2 * a * c);
     }
-    int liczPole(){
-    int result =  (2 * a * b) + (2 * b * c )+(2 * a * c);
-        return(result);
+
+    int liczObj() {
+        return a * b * c;
     }
-    int liczObj(){
-        int result =  a*b*c;
-        return(result);}
 };
-
-class kula{
+//Zad.3,4
+class Kula {
 public:
     double pi = 3.14;
     int r;
 
-    kula(int sred){
+    Kula(int sred) {
         r = sred;
     }
-    int liczKul(){
-        pi = 3.14;
-        double result = 4 * pi * r * 2;
-        return(result);
-    }
-    int liczObj(){
-        double result = 4/3 * pow(pi,3);
-                return(result);
+
+    double liczKul() {
+        return 4 * pi * r * r;
     }
 
+    double liczObj() {
+        return (4.0 / 3.0) * pi * r * r * r;
+    }
 };
-class walec{
+//Zad.5,6
+class Walec {
 public:
     double pi = 3.14;
-    int r1,h;
-    walec(int sred,int wys){
-        r1 = sred;
+    int r, h;
+
+    Walec(int sred, int wys) {
+        r = sred;
         h = wys;
     }
-    int liczwal(){
-        double result = (2 * pi * r1 * h )+ 2 * pi * sqrt(pi);
-        return(result);
+
+    double liczwal() {
+        return (2 * pi * r * h) + (2 * pi * r * r);
     }
-    int liczobj(){
-        double result = pi * sqrt(r1) * h;
+
+    double liczobj() {
+        return pi * r * r * h;
     }
 };
-class stozek{
+//Zad.7,8
+class Stozek {
 public:
-    double pi =3.14;
-    int r2,l,h1;
+    double pi = 3.14;
+    int r, l, h;
 
-    stozek(int sred, int bok, int wys){
-        r2 = sred;
+    Stozek(int sred, int bok, int wys) {
+        r = sred;
         l = bok;
-        h1 = wys;
+        h = wys;
     }
-    int liczstoz(){
-        double result = pi * sqrt(r2) + pi * r2 * l;
-        return(result);
+
+    double liczstoz() {
+        return pi * r * (r + l);
     }
-    int liczobj(){
-        double result = 1/3 * pi * sqrt(r2)* h1;
-        return(result);
+
+    double liczobj() {
+        return (1.0 / 3.0) * pi * r * r * h;
     }
 };
-
+//Zad.9,10
 class FunkcjaKwadratowa {
 public:
     double a, b, c;
 
     FunkcjaKwadratowa(double _a, double _b, double _c) {
-    a=(_a);
-    b=(_b);
-    c=(_c) ;
+        a = _a;
+        b = _b;
+        c = _c;
     }
+
     double obliczWartosc(double x) {
         return a * x * x + b * x + c;
     }
 };
+
 int main() {
-    cout << "Podaj boki prostopadloscianu" << endl;
-    int a;
-    int b;
-    int c;
-    cout << "podaj srednice" << endl;
+    cout << "<<Zadanie 1,2>> " << endl;
+    cout << "Podaj boki prostopadloscianu: ";
+    int a, b, c;
+    cin >> a >> b >> c;
+    Prostopadloscian prostopadloscian(a, b, c);
+    cout << "Pole prostopadloscianu: " << prostopadloscian.liczPole() << endl;
+    cout << "Objetosc prostopadloscianu: " << prostopadloscian.liczObj() << endl;
+    cout << "<<Zadanie 3,4>> " << endl;
+    cout << "Podaj srednice kuli: ";
     int r;
-    cout << "podaj srednice i wysokosc  walca" << endl;
-    int r1;
-    int h;
-    cout << " podaj sred i bok stozka" << endl;
-    int r2;
-    int l;
-    int h1;
-    cout << "Podaj wspolczynniki funkcji kwadratowej (a, b, c): ";
-    double a1, b1, c1, x1;
-
-
-    cin >> a >> b>> c;
     cin >> r;
+    Kula kula(r);
+    cout << "Pole kuli: " << kula.liczKul() << endl;
+    cout << "Objetosc kuli: " << kula.liczObj() << endl;
+    cout << "<<Zadanie 5,6>> " << endl;
+    cout << "Podaj srednice i wysokosc walca: ";
+    int r1, h;
     cin >> r1 >> h;
-    cin >> r2 >> l >> h;
+    Walec walec(r1, h);
+    cout << "Pole walca: " << walec.liczwal() << endl;
+    cout << "Objetosc walca: " << walec.liczobj() << endl;
+    cout << "<<Zadanie 7,8>> " << endl;
+    cout << "Podaj srednice, bok i wysokosc stozka: ";
+    int r2, l, h1;
+    cin >> r2 >> l >> h1;
+    Stozek stozek(r2, l, h1);
+    cout << "Pole stozka: " << stozek.liczstoz() << endl;
+    cout << "Objetosc stozka: " << stozek.liczobj() << endl;
+    cout << "<<Zadanie 9,10>> " << endl;
+    cout << "Podaj wspolczynniki funkcji kwadratowej (a, b, c): ";
+    double a1, b1, c1;
     cin >> a1 >> b1 >> c1;
-    pprospopadloscian liczPole(a,b,c);
-    kula liczKul(r);
-    walec liczwal(r1,h);
-    stozek liczsto(r2,l, h1);
     FunkcjaKwadratowa funkcja(a1, b1, c1);
     cout << "Podaj punkt, w ktorym chcesz obliczyc wartosc funkcji: ";
+    double x1;
     cin >> x1;
     double wynik = funkcja.obliczWartosc(x1);
     cout << "Wartosc funkcji w punkcie " << x1 << " wynosi: " << wynik << endl;
 
-
-    cout << "objetosc to " << liczPole.liczObj() << endl;
-    cout << "pole to " << liczPole.liczPole() << endl;
-    cout << "pole kuli to" << liczKul.liczKul() << endl;
-    cout << "objetosc kuli to" << liczKul.liczObj()<< endl;
-    cout << "pole walca to " << liczwal.liczwal() << endl;
-    cout << "objetosc walca to " << liczwal.liczobj() << endl;
-    cout << "pole stozka to " << liczsto.liczstoz() << endl;
-    cout << "objetosc stozka to " << liczsto.liczobj() << endl;
-
-
     return 0;
-
-
-
 }
